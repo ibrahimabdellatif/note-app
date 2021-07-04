25df54ca987b197b2ef24ce1565fd93f2a2561cf
ibrahim abdellatif <ibrahimabdellatif186@gmail.com>
2021-07-05 01:01:30 +0200

    swipe to delete with dialog fragment

--- app/src/main/java/com/example/noteapp/MainActivity.java
+++ app/src/main/java/com/example/noteapp/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.noteapp;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -29,8 +36,8 @@ public class MainActivity extends AppCompatActivity {
         buttonAddNote.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this , addNoteActivity.class);
-                startActivityForResult(intent,ADD_NOTE_REQUEST);
+                Intent intent = new Intent(MainActivity.this, addNoteActivity.class);
+                startActivityForResult(intent, ADD_NOTE_REQUEST);
 
             }
         });
@@ -53,22 +60,77 @@ public class MainActivity extends AppCompatActivity {
                 adapter.setNotes(notes);
             }
         });
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0,
+                ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            //onMove for drag and drop function we not use it in app
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            //swipe to left or right to delete note
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+
+                //show dialog fragment before deleting any note
+                new AlertDialog.Builder(viewHolder.itemView.getContext())
+                        .setMessage("Are You Sure")
+                        .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                int position = viewHolder.getPosition();
+                                noteViewModel.delete(adapter.getNoteAt(position));
+                                Toast.makeText(MainActivity.this, "Note Deleted", Toast.LENGTH_SHORT).show();
+                            }
+                        }).setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        adapter.notifyItemChanged(viewHolder.getAdapterPosition());
+                        dialog.cancel();
+                        Toast.makeText(MainActivity.this, "Nothing Changed", Toast.LENGTH_SHORT).show();
+                    }
+                }).create().show();
+
+            }
+        }).attachToRecyclerView(recyclerView);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == ADD_NOTE_REQUEST && resultCode == RESULT_OK){
+        if (requestCode == ADD_NOTE_REQUEST && resultCode == RESULT_OK) {
             String title = data.getStringExtra(addNoteActivity.EXTRA_TITLE);
             String description = data.getStringExtra(addNoteActivity.EXTRA_DESCRIPTION);
-            int priority = data.getIntExtra(addNoteActivity.EXTRA_PRIORITY , 1);
-            
-            Note note = new Note(title , description , priority);
+            int priority = data.getIntExtra(addNoteActivity.EXTRA_PRIORITY, 1);
+
+            Note note = new Note(title, description, priority);
             noteViewModel.insert(note);
             Toast.makeText(this, "Note Saved", Toast.LENGTH_SHORT).show();
-            
-        }else {
+
+        } else {
             Toast.makeText(this, "Note not Saved", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.delete_all_notes:
+                noteViewModel.deleteAllNotes();
+                Toast.makeText(this, "All Notes Deleted", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
